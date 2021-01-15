0173d4b61159ae1462b6d93ad2df7a91ca95d6b8
jkordonski <jkordonski97@gmail.com>
2021-01-15 07:29:11 +0000

    zad1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,4 +6,28 @@
 #include <sstream>
 #include <string>
 
-// tutaj klasa Artysta
+class Artysta
+{
+  private:
+  std::string pseudonim;
+  unsigned  liczba_sluchaczy;
+  public:
+
+  Artysta() :pseudonim("Anonim"), liczba_sluchaczy(0){}
+  Artysta(const std:: string& s, unsigned n) :pseudonim(s), liczba_sluchaczy(n){}
+
+  void graj(std:: ostream& a) const
+  {
+    a = std::to_string(pseudonim) + ": " std::to_string(liczba_sluchaczy);
+  }
+
+const std::string& getPseudonim() const
+{
+  return pseudonim;
+}
+
+unsigned getSluchacze() const
+{
+  return liczba_sluchaczy;
+}
+}
